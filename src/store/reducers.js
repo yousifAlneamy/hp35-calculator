f8fddd02fea23569ea38effdb33f57ec86b7cafd
yousifAlneamy <yousif.aladdin@gmail.com>
2017-09-23 21:18:03 +0300

    Change stack-reducer to action mapper

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,145 +4,118 @@ import { combineReducers } from "redux";
 const degreesToRadians = degrees => degrees * Math.PI / 180.0;
 const radiansToDegrees = radians => radians * 180.0 / Math.PI;
 
-export const stack = (state = ["0", "0", "0", "0"], action) => {
-	let [x, y, z, t] = state;
-	let { payload, type } = action;
-	switch (type) {
-		case C.ADD_DIGIT: {
-			let newX;
-			if (x === "0" || x === "-0" || x === "+0") {
+const stackActionMap = {
+	[C.ADD_DIGIT]: ({ payload, x, y, z, t }) => {
+		let newX;
+		if (x === "0" || x === "-0" || x === "+0") {
+			if (payload === "0") {
+				return [x, y, z, t];
+			} else {
+				const isNegative = x.startsWith("-");
+				newX = isNegative ? "-" : "";
+				newX += payload;
+				return [newX, y, z, t];
+			}
+		}
+
+		if (x.includes("e")) {
+			const isEEXNegative = x.includes("e-");
+			if (x.endsWith("+0") || x.endsWith("-0")) {
 				if (payload === "0") {
-					return state;
+					return [x, y, z, t];
 				} else {
-					const isNegative = x.startsWith("-");
-					newX = isNegative ? "-" : "";
+					newX = x.substr(0, x.indexOf("e") + 1);
+					newX += isEEXNegative ? "-" : "+";
 					newX += payload;
 					return [newX, y, z, t];
 				}
 			}
 
-			if (x.includes("e")) {
-				const isEEXNegative = x.includes("e-");
-				if (x.endsWith("+0") || x.endsWith("-0")) {
-					if (payload === "0") {
-						return state;
-					} else {
-						newX = x.substr(0, x.indexOf("e") + 1);
-						newX += isEEXNegative ? "-" : "+";
-						newX += payload;
-						return [newX, y, z, t];
-					}
-				}
-
-				if (x.length - x.indexOf("e") <= 3) {
-					newX = x + payload;
-					return [newX, y, z, t];
-				} else {
-					return state;
-				}
-			}
-
-			newX = x + payload;
-			return [newX, y, z, t];
-		}
-
-		case C.SHIFT_UP:
-		case C.ENTER:
-			return [x, x, y, z];
-
-		case C.EXCHANGE_X_WITH_Y:
-			return [y, x, z, t];
-
-		case C.SET_X:
-			return [payload, x, y, z];
-
-		case C.ADD_PI:
-			return [Math.PI.toString(), x, y, z];
-
-		case C.ROLE_DOWN:
-			return [y, z, t, x];
-
-		case C.X_POW_Y:
-			return [Math.pow(Number(y), Number(x)).toString(), z, t, t];
-
-		case C.SHIFT_DOWN:
-			return [x, y, z, z];
-
-		case C.CLR:
-			return ["0", "0", "0", "0"];
-
-		case C.CLX:
-			return ["0", y, z, t];
-
-		case C.REC:
-			return [payload, x, y, z];
-
-		case C.EXE_ARITHMETIC_OPERATOR:
-			return [payload(Number(y), Number(x)).toString(), z, t, t];
-
-		case C.EXE_NON_TRI_FUNCTION: // ln - log - e^x sqrt
-			return [payload(Number(x)).toString(), y, z, t];
-
-		case C.EXE_TRI_FUNCTION: // sin - cos - tan
-			return [payload(degreesToRadians(Number(x))).toString(), y, z, z];
-
-		case C.EXE_A_TRI_FUNCTION: // asin - cos - atan
-			return [radiansToDegrees(payload(Number(x))).toString(), y, z, z];
-
-		case C.MUL_INVERSE_X:
-			return [(1 / Number(x)).toString(), y, z, t];
-
-		case C.CHS: {
-			const isEEX = x.includes("e");
-			let newX;
-			if (isEEX) {
-				const isEEXNegative = x.includes("e-");
-				let signIndex;
-				if (isEEXNegative) {
-					signIndex = x.lastIndexOf("-");
-					newX = x.substr(0, signIndex) + "+" + x.substr(signIndex + 1);
-				} else {
-					signIndex = x.lastIndexOf("+");
-					newX = x.substr(0, signIndex) + "-" + x.substr(signIndex + 1);
-				}
+			if (x.length - x.indexOf("e") <= 3) {
+				newX = x + payload;
 				return [newX, y, z, t];
-			}
-
-			if (x.startsWith("-")) {
-				newX = "+" + x.substr(1);
 			} else {
-				newX = "-" + (x.startsWith("+") ? x.substr(1) : x.substr(0));
+				return [x, y, z, t];
 			}
-			return [newX, y, z, t];
 		}
 
-		case C.ADD_DOT: {
-			if (x.includes("e")) {
-				return state;
-			}
-
-			if (x.includes(".")) {
-				return state;
-			}
-			const newX = x + ".";
-			return [newX, y, z, t];
-		}
-
-		case C.ADD_EEX: {
-			if (x.includes("e")) {
-				return state;
-			}
-			let newX;
-			if (x === "0" || x === "-0" || x === "+0") {
-				newX = x.substr(1) + "1e+0";
+		newX = x + payload;
+		return [newX, y, z, t];
+	},
+	[C.SHIFT_UP]: ({ x, y, z }) => [x, x, y, z],
+	[C.ENTER]: ({ x, y, z }) => [x, x, y, z],
+	[C.EXCHANGE_X_WITH_Y]: ({ x, y, z, t }) => [y, x, z, t],
+	[C.SET_X]: ({ payload, x, y, z }) => [payload, x, y, z],
+	[C.ADD_PI]: ({ x, y, z }) => [Math.PI.toString(), x, y, z],
+	[C.ROLE_DOWN]: ({ x, y, z, t }) => [y, z, t, x],
+	[C.X_POW_Y]: ({ x, y, z, t }) => [Math.pow(Number(y), Number(x)).toString(), z, t, t],
+	[C.SHIFT_DOWN]: ({ x, y, z }) => [x, y, z, z],
+	[C.CLR]: () => ["0", "0", "0", "0"],
+	[C.CLX]: ({ y, z, t }) => ["0", y, z, t],
+	[C.REC]: ({ payload, x, y, z }) => [payload, x, y, z],
+	[C.EXE_ARITHMETIC_OPERATOR]: ({ payload, x, y, z, t }) => [payload(Number(y), Number(x)).toString(), z, t, t],
+	[C.EXE_NON_TRI_FUNCTION]: ({ payload, x, y, z, t }) => [payload(Number(x)).toString(), y, z, t], // ln - log - e^x sqrt
+	[C.EXE_TRI_FUNCTION]: ({ payload, x, y, z, t }) => [payload(degreesToRadians(Number(x))).toString(), y, z, z], // sin - cos - tan
+	[C.EXE_A_TRI_FUNCTION]: ({ payload, x, y, z, t }) => [radiansToDegrees(payload(Number(x))).toString(), y, z, z], // asin - cos - atan
+	[C.MUL_INVERSE_X]: ({ x, y, z, t }) => [(1 / Number(x)).toString(), y, z, t],
+	[C.CHS]: ({ x, y, z, t }) => {
+		const isEEX = x.includes("e");
+		let newX;
+		if (isEEX) {
+			const isEEXNegative = x.includes("e-");
+			let signIndex;
+			if (isEEXNegative) {
+				signIndex = x.lastIndexOf("-");
+				newX = x.substr(0, signIndex) + "+" + x.substr(signIndex + 1);
 			} else {
-				newX = x + "e+0";
+				signIndex = x.lastIndexOf("+");
+				newX = x.substr(0, signIndex) + "-" + x.substr(signIndex + 1);
 			}
 			return [newX, y, z, t];
 		}
-		default:
-			return state;
+
+		if (x.startsWith("-")) {
+			newX = "+" + x.substr(1);
+		} else {
+			newX = "-" + (x.startsWith("+") ? x.substr(1) : x.substr(0));
+		}
+		return [newX, y, z, t];
+	},
+	[C.ADD_DOT]: ({ x, y, z, t }) => {
+		if (x.includes("e")) {
+			return [x, y, z, t];
+		}
+
+		if (x.includes(".")) {
+			return [x, y, z, t];
+		}
+		const newX = x + ".";
+		return [newX, y, z, t];
+	},
+	[C.ADD_EEX]: ({ x, y, z, t }) => {
+		if (x.includes("e")) {
+			return [x, y, z, t];
+		}
+		let newX;
+		if (x === "0" || x === "-0" || x === "+0") {
+			newX = x.substr(1) + "1e+0";
+		} else {
+			newX = x + "e+0";
+		}
+		return [newX, y, z, t];
 	}
+};
+
+export const stack = (state = ["0", "0", "0", "0"], action) => {
+	const [x, y, z, t] = state;
+	const { type, payload } = action;
+	const params = { x, y, z, t, payload };
+
+	if (stackActionMap.hasOwnProperty(type)) {
+		return stackActionMap[type](params);
+	}
+
+	return state;
 };
 
 export const stackShift = (state = false, action) => {
